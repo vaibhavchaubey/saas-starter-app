02b117b8d7a147769b35612c75d10b1ff85c8809
Vaibhav <vaibhavchaubey786@gmail.com>
2024-12-25 01:32:10 +0530

    refactor ESLint configuration to disable unused variable rules and improve code clarity

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,21 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'off', // Completely disable the rule
+        {
+          varsIgnorePattern: '^_', // Ignore variables prefixed with '_'
+          argsIgnorePattern: '^_', // Ignore arguments prefixed with '_'
+          vars: 'all', // Apply to all variables
+          args: 'after-used', // Ignore unused arguments after they are used
+        },
+      ],
+      '@typescript-eslint/no-unused-vars-experimental': 'off', // Disable the experimental version of the rule (if applicable)
+    },
+  },
 ];
 
 export default eslintConfig;
